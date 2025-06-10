d321391475daaf22c7fdb428856ed45b77e1fd8b
yaskho <135612950+yaskho@users.noreply.github.com>
2025-06-10 04:16:15 +0100

    Add files via upload

---
+++ 1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from calculator import add, sub, mul, div
+a = 10
+b = 5
+if __name__ == "__main__":
+    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{a} - {b} = {sub(a, b)}")
+    print(f"{a} * {b} = {mul(a, b)}")
+    print(f"{a} / {b} = {div(a, b)}")
