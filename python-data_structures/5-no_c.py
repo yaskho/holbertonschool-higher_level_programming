4f1a595af72053aa11510ff7b8b60e9b2e34ec43
yaskho <yassineholberton@gmail.com>
2025-06-18 12:41:09 +0100

    https://github.com/yaskho/holbertonschool-higher_level_programming.git

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for char in my string:
+        if char != 'c' and char != 'c':
+            new string += char
+        return new_string
