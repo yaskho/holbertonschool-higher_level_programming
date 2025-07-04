f9b77c143f21779140adbe3d0f8affe56c535871
yaskho <yassineholberton@gmail.com>
2025-07-04 12:17:17 +0100

    task_02_requests.py

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,34 @@
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    print(f"Status Code: {response.status_code}")
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post['title'])
+
+
+def fetch_and_save_posts():
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    if response.status_code == 200:
+        posts = response.json()
+        data = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
+        with open('posts.csv', 'w', newline='') as csvfile:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(data)
+
+
+# Example usage
+if __name__ == "__main__":
+    fetch_and_print_posts()
+    fetch_and_save_posts()
+
+
+if __name__ == "__main__":
+    fetch_and_print_posts()
+    fetch_and_save_posts()
