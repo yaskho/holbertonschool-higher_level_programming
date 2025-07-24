71fe58553dd86a1ac165baf8295340c2cc317fc0
yaskho <yassineholberton@gmail.com>
2025-07-24 18:18:58 +0100

    jdhj

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""Module that defines a Square with private size."""
+
+
 class Square:
+    """Represents a square with a private size attribute."""
+
     def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
