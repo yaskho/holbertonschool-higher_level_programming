280d8da90850888019efabb90b983040faf55025
yaskho <yassineholberton@gmail.com>
2025-07-30 19:52:28 +0100

    hbdhb

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
--- List all shows and their genres, show NULL if no genre linked, ordered by show title and genre name
+-- Lists all shows and their linked genres from the hbtn_0d_tvshows database
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
