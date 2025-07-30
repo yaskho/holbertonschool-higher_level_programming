4509734e8c7b7a616ff5e6036193c5dca12b2858
yaskho <yassineholberton@gmail.com>
2025-07-30 19:48:54 +0100

    fkd

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
--- List all genres of the show Dexter, sorted by genre name ascending
-SELECT genres.name
-FROM genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.tv_show_id = tv_shows.id
+-- Lists all genres of the show Dexter
+SELECT tv_genres.name
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY genres.name ASC;
+ORDER BY tv_genres.name ASC;
