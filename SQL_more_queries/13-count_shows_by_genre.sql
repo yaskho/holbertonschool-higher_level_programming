73452138fac67afea9e0e10a57fa6f2bf44402fc
yaskho <yassineholberton@gmail.com>
2025-07-30 19:47:24 +0100

    dgvg

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
--- List all genres and the number of shows linked to each, excluding genres with zero shows
-SELECT genres.name AS genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
-FROM genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-GROUP BY genres.name
+-- Lists all genres and number of shows linked to each (only those with at least one show)
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.id
 HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
