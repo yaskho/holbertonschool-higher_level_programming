57631a51a5a9e129917bc633b78806d4311567cf
yaskho <yassineholberton@gmail.com>
2025-07-30 19:43:20 +0100

    dndn

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
--- List all shows with their genre_id, NULL if no genre linked
+-- Lists all shows and their genre_id (NULL if no genre)
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
