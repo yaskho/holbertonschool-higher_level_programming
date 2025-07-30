4e3719ea635bd0be21eb27ed5bc6e57f97120cd0
yaskho <yassineholberton@gmail.com>
2025-07-30 19:35:44 +0100

    njfd

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
--- List all shows with at least one genre linked
+-- List all shows with at least one genre linked: title - genre_id, sorted by title and genre_id
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows, tv_show_genres
 WHERE tv_shows.id = tv_show_genres.tv_show_id
