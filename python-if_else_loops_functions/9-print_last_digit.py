2edf866c3540ef1cdae4585542fcab6dabb90a9e
yaskho <yassineholberton@gmail.com>
2025-07-31 11:38:09 +0100

    nbd

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-def print_last_digit(number)
-    if number < 0:
-        number = -number
-    last = number % 10
+def print_last_digit(number):
+    last = abs(number) % 10
     print(last, end="")
     return last
