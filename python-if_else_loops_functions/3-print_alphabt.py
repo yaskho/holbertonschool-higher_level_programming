85eab5a6d54b2a64f3aeb773b618626363b1dd3e
yaskho <yassineholberton@gmail.com>
2025-07-31 11:18:07 +0100

    njb

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("{}".format("".join(chr(i) for i in range(97, 123) if i != 101 and i != 113)), end="")
+print("{}".format("".join(
+    chr(i) for i in range(97, 123) if i != 101 and i != 113)), end="")
