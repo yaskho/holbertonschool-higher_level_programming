38ff289a5d5545c3a7e07694f6f223d8c29a9b24
yaskho <yassineholberton@gmail.com>
2025-07-31 11:14:59 +0100

    vff

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print("{0}".format("".join([chr(c) for c in range(97,123) if c not in [101,113]])), end="")
+print("{}".format("".join(chr(i) for i in range(97, 123) if i != 101 and i != 113)), end="")
