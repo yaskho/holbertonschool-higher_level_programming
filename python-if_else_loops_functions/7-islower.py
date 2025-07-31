c1c786d6e0894e47110a122718a3767a84f021b6
yaskho <yassineholberton@gmail.com>
2025-07-31 11:26:05 +0100

    nbh

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-#code asci verification
-    if ord(c) >= 97 and ord(c) <=122:
-        return True
-    else:
-        return False
+    return ord(c) >= 97 and ord(c) <= 122
