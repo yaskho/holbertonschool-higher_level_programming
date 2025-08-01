27ecfbe3e24bfe4844c07edd670c3feab00c6e7e
yaskho <yassineholberton@gmail.com>
2025-08-01 01:47:20 +0100

    hbv

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_string = ""
-    for char in my string:
-        if char != 'c' and char != 'c':
-            new string += char
-        return new_string
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
