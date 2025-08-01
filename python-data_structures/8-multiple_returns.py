7c8f35ee81ed22bfacdd6df7cf4bd85aed03740d
yaskho <yassineholberton@gmail.com>
2025-08-01 01:56:38 +0100

    jnfn

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
-    if not sentence:
-        return (0, None)
-    return (len(sentence), sentence[0])
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    max_val = my_list[0]
+    for i in my_list[1:]:
+        if i > max_val:
+            max_val = i
+    return max_val
