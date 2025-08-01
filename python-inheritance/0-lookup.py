e6f001e89260d1bf0a68acce1c8cbaedc158f26b
yaskho <yassineholberton@gmail.com>
2025-08-01 02:17:58 +0100

    hgh

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""
+Module that defines the function lookup(obj):
+Returns a list of available attributes and methods of an object.
+"""
+
 def lookup(obj):
     return dir(obj)
