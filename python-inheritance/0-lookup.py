e9954dff04594c051a48c90d1d913c673f90e3e9
yaskho <yassineholberton@gmail.com>
2025-08-01 02:16:20 +0100

    hhg

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-"""Module that provides a function to list available attributes and methods of an object."""
-
 def lookup(obj):
-    """Returns the list of available attributes and methods of an object."""
     return dir(obj)
