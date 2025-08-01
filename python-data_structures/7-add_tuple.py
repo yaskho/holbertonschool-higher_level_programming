66726174ec1baff30203d4f3aecb1520bd1829c0
yaskho <yassineholberton@gmail.com>
2025-08-01 01:54:37 +0100

    jbnhb

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -5,5 +5,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     a2 = tuple_a[1] if len(tuple_a) >= 2 else 0
     b1 = tuple_b[0] if len(tuple_b) >= 1 else 0
     b2 = tuple_b[1] if len(tuple_b) >= 2 else 0
-    
+
     return (a1 + b1, a2 + b2)
