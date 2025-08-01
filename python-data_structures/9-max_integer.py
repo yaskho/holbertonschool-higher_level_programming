02b2f8720a9acb82a5aab3c84b5a0a7c01d2cc7d
yaskho <yassineholberton@gmail.com>
2025-08-01 01:58:59 +0100

    hvg

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:
+    if len(my_list) == 0:
         return None
+
     max_val = my_list[0]
-    for num in my list[1:]:
-        if num > max_val:
-            max_val = num
-        return max_val
+    for i in my_list[1:]:
+        if i > max_val:
+            max_val = i
+    return max_val
