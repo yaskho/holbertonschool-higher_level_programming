5a0b87415b87bef58d0ec3d0893758014fb388d9
yaskho <yassineholberton@gmail.com>
2025-08-01 02:05:40 +0100

    jbhv

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,4 +2,7 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
-    return my_list[:idx] + my_list[idx+1:]
+    for i in range(idx, len(my_list) - 1):
+        my_list[i] = my_list[i + 1]
+    my_list[:] = my_list[:-1]
+    return my_list
