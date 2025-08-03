087cd4e07dd915283b1c7d2b1259af8264725240
yaskho <yassineholberton@gmail.com>
2025-08-04 00:46:38 +0100

    jbj

--- python-object_relational_mapping/4-cities_by_state.sql
+++ python-object_relational_mapping/4-cities_by_state.sql
@@ -7,7 +7,9 @@ CREATE TABLE IF NOT EXISTS states (
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
 );
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+
+INSERT INTO states (name) VALUES 
+("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
 
 CREATE TABLE IF NOT EXISTS cities ( 
     id INT NOT NULL AUTO_INCREMENT, 
@@ -16,7 +18,8 @@ CREATE TABLE IF NOT EXISTS cities (
     PRIMARY KEY (id),
     FOREIGN KEY(state_id) REFERENCES states(id)
 );
-INSERT INTO cities (state_id, name) VALUES
+
+INSERT INTO cities (state_id, name) VALUES 
 (1, "San Francisco"), (1, "San Jose"), (1, "Los Angeles"), (1, "Fremont"), (1, "Livermore"),
 (2, "Page"), (2, "Phoenix"),
 (3, "Dallas"), (3, "Houston"), (3, "Austin"),
