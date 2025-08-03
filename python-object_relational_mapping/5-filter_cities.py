90d60e18a161a5256aab5e305adaca6384f6a43f
yaskho <yassineholberton@gmail.com>
2025-08-04 00:40:05 +0100

    gc

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -3,14 +3,11 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    # Get arguments
     user, passwd, db_name, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
 
-    # Connect to database
     db = MySQLdb.connect(host="localhost", port=3306, user=user, passwd=passwd, db=db_name)
     cursor = db.cursor()
 
-    # SQL query to get cities of the state with given name
     query = """
     SELECT cities.name FROM cities
     JOIN states ON cities.state_id = states.id
@@ -19,10 +16,7 @@ if __name__ == "__main__":
     """
     cursor.execute(query, (state_name,))
 
-    # Fetch all city names
     cities = cursor.fetchall()
-
-    # Print city names comma separated (no output if no cities found)
     print(", ".join(city[0] for city in cities))
 
     cursor.close()
