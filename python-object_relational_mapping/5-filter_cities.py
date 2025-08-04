7929b63b939f0b1a75c09d0c36e291bcab103bbb
yaskho <yassineholberton@gmail.com>
2025-08-04 22:46:28 +0100

    bhbh

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,24 +1,39 @@
 #!/usr/bin/python3
-import sys
+"""
+Lists all states where the name matches in the database hbtn_0e_0_usa
+"""
+
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    user, passwd, db_name, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=user, passwd=passwd, db=db_name)
-    cursor = db.cursor()
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
 
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name,
+        charset="utf8"
+    )
+
+    cur = db.cursor()
     query = """
-    SELECT cities.name FROM cities
+    SELECT cities.name
+    FROM cities
     JOIN states ON cities.state_id = states.id
     WHERE states.name = %s
     ORDER BY cities.id ASC
     """
-    cursor.execute(query, (state_name,))
-    rows = cursor.fetchall()
+    cur.execute(query, (state_name,))
 
-    if rows:
-        print(", ".join(row[0] for row in rows))
+    cities = [row[0] for row in cur.fetchall()]
+    print(", ".join(cities))
 
-    cursor.close()
+    cur.close()
     db.close()
