b8c62ead3c5ab27d2e967eee6fd32afc023a085d
yaskho <yassineholberton@gmail.com>
2025-08-04 22:30:33 +0100

    jnjn

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -12,7 +12,7 @@ Functions:
 
 def say_my_name(first_name, last_name=""):
     """
-    Prints My name is <first name> <last name>.
+    Prints "My name is <first_name> <last_name>".
 
     Args:
         first_name (str): The first name.
@@ -21,12 +21,18 @@ def say_my_name(first_name, last_name=""):
     Raises:
         TypeError: If first_name or last_name is not a string.
 
-    Prints:
-        A string in the format "My name is <first_name> <last_name> ".
+    Example:
+        >>> say_my_name("John", "Smith")
+        My name is John Smith
+        >>> say_my_name("Bob")
+        My name is Bob
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    print("My name is {} {} ".format(first_name, last_name))
+    if last_name:
+        print(f"My name is {first_name} {last_name}")
+    else:
+        print(f"My name is {first_name}")
