a8a8a34a9216461254845282bbc9bc60ecac4899
yaskho <yassineholberton@gmail.com>
2025-08-04 22:28:51 +0100

    hvg

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -12,18 +12,21 @@ Functions:
 
 def say_my_name(first_name, last_name=""):
     """
-    Prints 'My name is <first name> <last name>'.
+    Prints My name is <first name> <last name>.
 
     Args:
-        first_name (str): First name.
-        last_name (str, optional): Last name. Defaults to "".
+        first_name (str): The first name.
+        last_name (str, optional): The last name. Defaults to "".
 
     Raises:
         TypeError: If first_name or last_name is not a string.
+
+    Prints:
+        A string in the format "My name is <first_name> <last_name> ".
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    
-    print(f"My name is {first_name} {last_name}".strip())
+
+    print("My name is {} {} ".format(first_name, last_name))
