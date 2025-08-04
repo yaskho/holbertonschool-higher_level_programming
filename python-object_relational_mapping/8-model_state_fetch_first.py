7b957bfd9523cc06ee658f407629d646293ac076
yaskho <yassineholberton@gmail.com>
2025-08-04 23:10:58 +0100

    jnjkn

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,23 +1,35 @@
+#!/usr/bin/python3
+"""
+Script that prints the first State object from the database.
+
+Usage: ./8-model_state_fetch_first.py <mysql_username> <mysql_password> <database_name>
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
 def main():
-    if len(sys.argv) != 4:
-        print("Usage: ./8-model_state_fetch_first.py <mysql_username> <mysql_password> <database_name>")
-        sys.exit(1)
+ if len(sys.argv) != 4:
+ print("Usage: ./8-model_state_fetch_first.py <mysql_username> <mysql_password> <database_name>")
+ sys.exit(1)
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+ username = sys.argv[1]
+ password = sys.argv[2]
+ database = sys.argv[3]
 
-    engine = create_engine(f'mysql://{username}:{password}@localhost:3306/{database}')
+ engine = create_engine(f'mysql://{username}:{password}@localhost:3306/{database}')
+ Session = sessionmaker(bind=engine)
+ session = Session()
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+ first_state = session.query(State).order_by(State.id.asc()).first()
 
-    first_state = session.query(State).order_by(State.id.asc()).first()
+ if first_state:
+ print(f"{first_state.id}: {first_state.name}")
+ else:
+ print("Nothing")
 
-    if first_state:
-        print(f"{first_state.id}: {first_state.name}")
-    else:
-        print("Nothing")
+ session.close()
 
 if __name__ == "__main__":
-    main()
+ main()
