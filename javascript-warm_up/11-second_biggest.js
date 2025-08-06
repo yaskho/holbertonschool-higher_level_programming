29c32c947ff062ae81180ac7d4e2d5b6801f2585
yaskho <yassineholberton@gmail.com>
2025-08-06 13:57:29 +0100

    jbjh

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+const args = process.argv.slice(2).map(x => parseInt(x));
+
+if (args.length < 2) {
+  console.log(0);
+} else {
+  // Remove duplicates and sort descending
+  const uniqueSorted = [...new Set(args)].sort((a, b) => b - a);
+  console.log(uniqueSorted.length < 2 ? 0 : uniqueSorted[1]);
+}
